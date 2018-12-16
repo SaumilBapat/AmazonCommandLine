b13996f36a77f79f0fa50b72d9fb82d2a75f0066
SaumilBapat <sbapat@salesforce.com>
2018-12-15 21:46:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// Optional Entry point to select if you're a customer | manager | supervisor
+
+// Require Inquirer
 const inquirer = require('inquirer');
 
-
+// Main Logic
 async function main() {
+  // Prompt the user for their role
   const options = ["I'm a Customer", "I'm a Manager", "I'm a Supervisor"];
   const user = await inquirer.prompt([
     { type: 'list', name: 'option', choices: options },
   ]);
+
+  // Execute the right script based on user input
   switch (user.option) {
     case "I'm a Customer":
     require('./resources/javascript/bamazonCustomer.js');
